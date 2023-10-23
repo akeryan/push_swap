d1604149ecc91d21bb182b3ece159cc21c8badc8
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-10-23 20:14:41 +0400

    Reserve copy: changing lis() function
    modifying to make from list based to array based (for convenience
    purposes)

--- src/lis.c
+++ src/lis.c
@@ -5,12 +5,15 @@ int lis(Stack *stack) {
 	int i;
 	int j;
 	int *d;
+	int *p;
+	//int *lis_ix;
 	int max_len;
 	Node *i_node;
 	Node *j_node;
 	
-	d = (int*)calloc(stack->length, sizeof(int));
-	if(!d) {
+	d = (int*)ft_calloc(stack->length, sizeof(int));
+	p = (int*)ft_calloc(stack->length, sizeof(int));
+	if(!d || !p) {
 		fprintf(stderr, "Error - lis(): Allocation of memory has failed\n");
 		exit(0);
 	}
@@ -18,6 +21,7 @@ int lis(Stack *stack) {
 	i = 0;
 	while(i < stack->length) {
 		d[i] = 1;
+		p[i] = -1;
 		i++;
 	}
 
@@ -27,8 +31,10 @@ int lis(Stack *stack) {
 		j = 0;
 		j_node = stack->top;
 		while(j < i) {
-			if(j_node->data < i_node->data)
-				d[i] = max(d[i], d[j] + 1);
+			if(j_node->data < i_node->data && d[i] < d[j] + 1) {
+				d[i] = d[j] + 1;
+				p[i] = j;
+			}
 			j++;
 			j_node = j_node->next;
 		}
@@ -43,10 +49,29 @@ int lis(Stack *stack) {
 	}
 
     max_len = d[0];
+	int max_index = 0;
 	i = 0;
 	while(i < stack->length) {
-		max_len = max(max_len, d[i]);
+		if(d[i] > max_len) {
+			max_len = d[i];
+			max_index = i;
+		}
 		i++;
 	}
+
+	i = 0;
+	while(i < stack->length) {
+		printf("p[%d]: %d\n", i, p[i]);
+		i++;
+	}
+
+	//lis_ix = (int *)ft_calloc(max_len, sizeof(int));
+	//printf("max index = %d\n", max_index);
+	//i = max_index - 1;
+	//int pos = max_index;
+	////lis_ix[i--] = d[max_index];
+	//while(d[i] != 1) {
+		//lis_ix[i] = a[pos];
+	//}
     return max_len;
 }
