4f10f0269890f6db12297e8c49c96a21cab7ceae
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-10-23 20:45:57 +0400

    LIS() (longest increasing sequence): update..

--- src/lis.c
+++ src/lis.c
@@ -6,18 +6,30 @@ int lis(Stack *stack) {
 	int j;
 	int *d;
 	int *p;
+	int *s_arr; //stack_array: array for keeping the stack
 	//int *lis_ix;
-	int max_len;
+	int max_lis_len;
 	Node *i_node;
-	Node *j_node;
 	
+	s_arr = (int*)ft_calloc(stack->length, sizeof(int));
 	d = (int*)ft_calloc(stack->length, sizeof(int));
 	p = (int*)ft_calloc(stack->length, sizeof(int));
-	if(!d || !p) {
+	if(!d || !p || !s_arr) {
 		fprintf(stderr, "Error - lis(): Allocation of memory has failed\n");
 		exit(0);
 	}
 
+// copy stack to the s_arr array
+	i = 0;
+	i_node = stack->top;
+	while(1) {
+		s_arr[i++] = i_node->data;
+		if(i_node->next == stack->top)
+			break;
+		i_node = i_node->next;
+	}
+
+// initialize 'd' and 'p' arrays
 	i = 0;
 	while(i < stack->length) {
 		d[i] = 1;
@@ -25,36 +37,36 @@ int lis(Stack *stack) {
 		i++;
 	}
 
+ // calculate 'lis' for each i (0-n) and save in 'p' array under corresponding inex 'i'
 	i = 0;
 	i_node = stack->top;
 	while(i < stack->length) {
 		j = 0;
-		j_node = stack->top;
 		while(j < i) {
-			if(j_node->data < i_node->data && d[i] < d[j] + 1) {
+			if(s_arr[j] < s_arr[i] && d[i] < d[j] + 1) {
 				d[i] = d[j] + 1;
 				p[i] = j;
 			}
 			j++;
-			j_node = j_node->next;
 		}
 		i++;
-		i_node = i_node->next;
 	}
 	
+	// print values of array 'd'
 	i = 0;
 	while(i < stack->length) {
 		printf("d[%d]: %d\n", i, d[i]);
 		i++;
 	}
 
-    max_len = d[0];
-	int max_index = 0;
+	// find the maximum 'lis' length (max_lis_len) in 'd' array and corresponding index (max_lis_ix)
+    max_lis_len = d[0];
+	int max_lis_ix = 0;
 	i = 0;
 	while(i < stack->length) {
-		if(d[i] > max_len) {
-			max_len = d[i];
-			max_index = i;
+		if(d[i] > max_lis_len) {
+			max_lis_len = d[i];
+			max_lis_ix = i;
 		}
 		i++;
 	}
@@ -73,5 +85,5 @@ int lis(Stack *stack) {
 	//while(d[i] != 1) {
 		//lis_ix[i] = a[pos];
 	//}
-    return max_len;
+    return max_lis_len;
 }
