00c7a98774f4ba7431d5128a734ffd0fc3d6117a
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-10-26 17:47:12 +0400

    INJECT_BACK() added

---
+++ src/inject.c
@@ -0,0 +1,17 @@
+#include "../include/push_swap.h"
+
+/*	- This function injects elements from stack 'b' to stack 'a' in there correct locatoins
+	- Assumption is that stack 'a' is sorted (in an ascending order)
+*/
+
+int inject_back(Twix *twix) {
+	int *rotations;
+	int steps;
+
+	steps = 0;
+	while(twix->b.length > 0) {
+		rotations = cheapest_push(&twix->a, &twix->b);
+		steps += ps_push(twix, rotations);
+	}
+	return (steps);
+}
