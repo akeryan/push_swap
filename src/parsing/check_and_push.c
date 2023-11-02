8430ffb6ca650e75f59d74b88ee0e4f3d12db2dc
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2023-11-02 10:11:10 +0400

    FINAL COMMIT V4
    - all the leaks are cleaned

--- src/parsing/check_and_push.c
+++ src/parsing/check_and_push.c
@@ -6,14 +6,15 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 12:27:01 by akeryan           #+#    #+#             */
-/*   Updated: 2023/11/01 22:13:06 by akeryan          ###   ########.fr       */
+/*   Updated: 2023/11/02 09:47:55 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-int		ft_strcmp(const char *s1, const char *s2);
-char	*num_to_str(const char *str);
+int			ft_strcmp(const char *s1, const char *s2);
+char		*num_to_str(const char *str);
+static void	duo_free(char *s1, char *s2);
 
 //checks each value of '**str' and if valid pushes to the 'stack'
 void	check_and_push(char **str, t_stack *stack)
@@ -26,25 +27,21 @@ void	check_and_push(char **str, t_stack *stack)
 	i = 0;
 	while (str[i] != NULL)
 		i++;
-	while (42)
+	while (--i >= 0)
 	{
-		push_candidate_number = ft_atoi(str[--i]);
+		push_candidate_number = ft_atoi(str[i]);
 		itoa_value = ft_itoa(push_candidate_number);
 		check_allocation(itoa_value);
 		num_str = num_to_str(str[i]);
 		if (ft_strcmp(itoa_value, num_str) != 0)
 		{
 			printf("Error\n");
-			free(itoa_value);
-			free(num_str);
+			duo_free(itoa_value, num_str);
 			exit(1);
 		}
 		if (!is_duplicate(stack, push_candidate_number))
 			push(stack, push_candidate_number);
-		free(itoa_value);
-		free(num_str);
-		if (i == 0)
-			break ;
+		duo_free(itoa_value, num_str);
 	}
 }
 
@@ -66,29 +63,31 @@ char	*num_to_str(const char *str)
 {
 	int		is_minus;
 	char	*substr;
+	char	*new_str;
 
 	is_minus = 0;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
 	if (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
+		if (*str++ == '-')
 			is_minus = 1;
-		str++;
-	}
 	while (*str == '0')
-	{
-		if (ft_strlen(str) == 1)
+		if (ft_strlen(str++) == 1)
 			return (ft_itoa(0));
-		str++;
-	}
 	substr = ft_strdup(str);
 	if (!ft_isdigit(*str))
 		return (substr);
 	if (is_minus == 1)
-	{ char *newstr = ft_strjoin("-", substr);
-		free(substr);
-		return (newstr);
+	{
+		new_str = ft_strjoin("-", substr);
+		ft_free(substr);
+		return (new_str);
 	}
 	return (substr);
+}
+
+static void	duo_free(char *s1, char *s2)
+{
+	ft_free(s1);
+	ft_free(s2);
 }
